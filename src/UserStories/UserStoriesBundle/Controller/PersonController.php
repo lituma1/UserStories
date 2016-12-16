058ab18b98e4a93ca9c0e8355a62e5b51814137c
Piotr Popkiewicz <lituma1@gmail.com>
2016-12-16 23:41:39 +0100

    Improve PersonController

--- src/UserStories/UserStoriesBundle/Controller/PersonController.php
+++ src/UserStories/UserStoriesBundle/Controller/PersonController.php
@@ -9,57 +9,93 @@ use UserStories\UserStoriesBundle\Entity\Person;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class PersonController extends Controller
-{
+class PersonController extends Controller {
+
     /**
-     * @Route("/new")
+     * @Route("/new", name="new_action")
      * @Method({"GET", "POST"})
      */
-    public function newAction()
-    {
+    public function newAction(Request $request) {
+        $person = new Person();
+        $form = $this->createFormBuilder($person)
+                ->add('name', 'text', array('label' => 'Imię'))
+                ->add('surname', 'text', array('label' => 'Nazwisko'))
+                ->add('description', 'text', array('label' => 'Opis'))
+                ->add('save', 'submit', array('label' => 'Potwierdź'))
+                ->getForm();
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+
+            $person = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($person);
+
+            $em->flush();
+            return $this->render('USBundle:Person:show_one.html.twig', array(
+                        'person' => $person));
+        }
         return $this->render('USBundle:Person:new.html.twig', array(
-            // ...
-        ));
+                    'form' => $form->createView()));
     }
 
     /**
      * @Route("/{id}/modify")
      * @Method({"GET", "POST"})
      */
-    public function modifyAction($id)
-    {
+    public function modifyAction($id) {
         return $this->render('USBundle:Person:modify.html.twig', array(
-            // ...
+                        // ...
         ));
     }
 
     /**
      * @Route("/{id}/delete")
      */
-    public function deleteAction($id)
-    {
-        return $this->render('USBundle:Person:delete.html.twig', array(
-            // ...
+    public function deleteAction($id) {
+        $repository = $this->getDoctrine()->getRepository('USBundle:Person');
+        $person = $repository->find($id);
+
+        if ($person) {
+        
+            $em = $this->getDoctrine()->getManager(); 
+            $em->remove($person);
+            $em->flush();
+            return $this->render('USBundle:Person:delete.html.twig', array(
+                        'tekst' => 'usunięto kontakt'
         ));
+        } else {
+            return $this->render('USBundle:Person:delete.html.twig', array(
+                        'tekst' => 'nie ma takiego kontaktu'));
+        }
+
+
+        
     }
 
     /**
-     * @Route("/")
+     * @Route("/main", name="main_page")
      */
-    public function showAllAction()
-    {
-        return $this->render('USBundle:Person:show.html.twig', array(
-            // ...
-        ));
+    public function showAllAction() {
+
+        $repository = $this->getDoctrine()->getRepository('USBundle:Person');
+        $persons = $repository->findAll();
+
+        return $this->render('USBundle:Person:showAll.html.twig', array('persons' => $persons)
+        );
     }
+
     /**
      * @Route("/{id}")
      */
     public function showOneByIdAction($id) {
+
+        $repository = $this->getDoctrine()->getRepository('USBundle:Person');
+        $person = $repository->find($id);
+
         return $this->render('USBundle:Person:show_one.html.twig', array(
-            // ...
+                    'person' => $person
         ));
     }
-    
 
 }
